ce5e3e748bc3d8f8a394a22f1a1353b80d1f11a6
efimo <train.supersonic@gmail.com>
2018-09-29 17:53:46 +0300

    dedicated classes for group and contact management

--- test/add_contact.py
+++ test/add_contact.py
@@ -13,10 +13,10 @@ def app(request):
 
 def test_add_contact(app):
     app.session.login(login="admin", password="secret")
-    app.create_contact(Contact(firstname="Nickolay", middlename="Igorevich", lastname="Efimov", nickname="kolya", title="Title", company="Company", photo="D:\\testpicture.jpg", address="Address", home_phone="HomePhone", work_phone="MobilePhone", mobile_phone="WorkPhone", fax_number="FaxNumber", email="email1@mail.ru", email2="email2@mail.ru", email3="email3@mail.ru", homepage="homepage.ru", birth_day="17", birth_month="November", birth_year="1995", anniversary_day="18", anniversary_month="December", anniversary_year="1996", group="gav1", address2="address", phone_number_2="HomeAddress", notes="Notes"))
+    app.contact.create(Contact(firstname="Nickolay", middlename="Igorevich", lastname="Efimov", nickname="kolya", title="Title", company="Company", photo="D:\\testpicture.jpg", address="Address", home_phone="HomePhone", work_phone="MobilePhone", mobile_phone="WorkPhone", fax_number="FaxNumber", email="email1@mail.ru", email2="email2@mail.ru", email3="email3@mail.ru", homepage="homepage.ru", birth_day="17", birth_month="November", birth_year="1995", anniversary_day="18", anniversary_month="December", anniversary_year="1996", group="gav1", address2="address", phone_number_2="HomeAddress", notes="Notes"))
     app.session.logout()
 
 def test_add_empty_contact(app):
     app.session.login(login="admin", password="secret")
-    app.create_contact(Contact(firstname="", middlename="", lastname="", nickname="", title="", company="", photo="D:\\testpicture.jpg", address="", home_phone="", work_phone="", mobile_phone="", fax_number="", email="", email2="", email3="", homepage="", birth_day="", birth_month="-", birth_year="", anniversary_day="", anniversary_month="-", anniversary_year="", group="", address2="", phone_number_2="", notes=""))
+    app.contact.create(Contact(firstname="", middlename="", lastname="", nickname="", title="", company="", photo="D:\\testpicture.jpg", address="", home_phone="", work_phone="", mobile_phone="", fax_number="", email="", email2="", email3="", homepage="", birth_day="", birth_month="-", birth_year="", anniversary_day="", anniversary_month="-", anniversary_year="", group="", address2="", phone_number_2="", notes=""))
     app.session.logout()
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -14,11 +14,11 @@ def app(request):
 
 def test_add_group(app):
     app.session.login(login="admin", password="secret")
-    app.create_group(Group(name="gav2", header="gav3", footer="gav4"))
+    app.group.create(Group(name="gav2", header="gav3", footer="gav4"))
     app.session.logout()
 
 
 def test_add_empty_group(app):
     app.session.login(login="admin", password="secret")
-    app.create_group(Group(name="", header="", footer=""))
+    app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
