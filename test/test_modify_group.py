ba15f4b31899cb45f058cf6cbd33ea3bf5f56a80
efimo <train.supersonic@gmail.com>
2018-10-05 22:11:26 +0300

    add fixture validation before each test

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -2,12 +2,8 @@ from model.group import Group
 
 
 def test_modify_group_name(app):
-    app.session.login(login="admin", password="secret")
     app.group.modify_first_group(Group(name="New group"))
-    app.session.logout()
-
 
 def test_modify_group_header(app):
-    app.session.login(login="admin", password="secret")
     app.group.modify_first_group(Group(header="header_edited"))
-    app.session.logout()
+
