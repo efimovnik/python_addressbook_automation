320cb9afac298dc323e4bea0109e56e6ba99df36
nikolayefimov <train.supersonic@gmail.com>
2018-10-13 01:37:17 +0300

    add check quantity of groups after edit, add and delete groups

--- fixture/group.py
+++ fixture/group.py
@@ -1,4 +1,4 @@
-
+from model.group import Group
 
 class GroupHelper:
 
@@ -77,3 +77,13 @@ class GroupHelper:
         wd = self.app.wd
         if not (wd.current_url.endswith("/group.php") and len(wd.find_elements_by_name("new")) > 0):
             wd.find_element_by_link_text("group page").click()
+
+    def get_group_list(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        groups = []
+        for element in wd.find_elements_by_css_selector("span.group"):
+            text = element.text
+            id = element.find_element_by_name("selected[]").get_attribute("value")
+            groups.append(Group(name=text, id=id))
+        return groups
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,13 +3,21 @@
 from model.group import Group
 
 def test_add_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="gav2", header="gav3", footer="gav4"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
 
 def test_add_empty_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="", header="", footer=""))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
 
 def test_add_contact_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="contact_group", header="contact_group", footer="contact_group"))
-
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
